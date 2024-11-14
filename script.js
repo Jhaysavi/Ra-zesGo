9e420c3905658ef1c8dcf4586a47255652bfcdb1
Jhay Savi <jansavinon@outlook.com>
2024-11-14 20:05:23 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function openApp() {
+    window.location.href = "https://www.figma.com/proto/DIqg1AFvoghH05JZFKlQNp/Hackton?node-id=12-92&node-type=frame&t=mWdW1WYSUFSQFeby-0&scaling=scale-down&content-scaling=fixed&page-id=0%3A1", "blank";
+}
+
+function openBot() {
+    // 
+    window.open("https://wa.me/numero-do-bot", "_blank");
+}
